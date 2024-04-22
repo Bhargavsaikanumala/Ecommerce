15c5d2269d7f03201e621e3ceb9e3aec3fb80e3a
Darshan V <55344738+Darshan-v18@users.noreply.github.com>
2024-04-22 09:50:25 +0530

    Update CartController.java

--- src/main/java/com/example/ecommerce/Controller/CartController.java
+++ src/main/java/com/example/ecommerce/Controller/CartController.java
@@ -15,7 +15,7 @@ public class CartController {
     private CartService cartService;
 
     @PostMapping("cart/add")
-    public ResponseEntity<String> addToCart(@RequestBody Long productId, @RequestBody int quantity) {
+    public ResponseEntity<String> addToCart(@RequestHeader Long productId, @RequestHeader int quantity) {
         boolean success = cartService.addToCart(productId, quantity);
         if (success) {
             return ResponseEntity.ok("Product added to cart successfully.");
